3dcd04287e8ab1b75ea6e3dba42dfdca02730b2c
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-01-16 15:13:47 +0800

    Time: 4 ms (24.79%) | Memory: 95.1 MB (75.20%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,25 +4,23 @@ public:
         int m = nums1.size();
         int n = nums2.size();
         int i = 0, j = 0, m1 = 0, m2 = 0;
-
-        // 查找中位数
-        for(int count = 0; count <= (m + n) / 2; ++count) {
-            // 如果两数组长度为偶数位，则保留2位
+        int mid = (m + n) / 2;
+        for(int c = 0; c <= mid; ++c) {
             m2 = m1;
-            if ( i < m && j < n) {
-                if (nums1[i] > nums2[j]) {
-                    m1 = nums2[j++];
-                } else {
+            if(i < m && j < n) {
+                if(nums1[i] < nums2[j]) {
                     m1 = nums1[i++];
+                } else {
+                    m1 = nums2[j++];
                 }
-            } else if ( i < m) {
+            } else if (i < m) {
                 m1 = nums1[i++];
             } else {
                 m1 = nums2[j++];
             }
         }
 
-        if ((m + n) % 2 == 1) {
+        if ((m+n) % 2 == 1) {
             return static_cast<double>(m1);
         }
         return (static_cast<double>(m1) + static_cast<double>(m2)) / 2.0;
