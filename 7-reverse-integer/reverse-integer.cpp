b107512bdcbf257c2b36381a312dd8dcbda0f070
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-01-19 16:11:22 +0800

    Time: 0 ms (100.00%) | Memory: 8.6 MB (49.38%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,40 +1,15 @@
 class Solution {
 public:
     int reverse(int x) {
-        bool flag = false;
-        int64_t ans = x;
-        if (ans < 0) {
-            flag = true;
-            ans = -ans;
-        }
-
-        vector<int> num;
-        bool first = true;
-        while(ans) {
-            int v = ans % 10;
-            ans /= 10;
-            if (v == 0 && first) {
-                continue;
+        int64_t ans = 0;
+        while(x) {
+            int v = x % 10;
+            ans = ans * 10 + v;
+            if (ans > INT_MAX || ans < INT_MIN) {
+                return 0;
             }
-            first = false;
-            num.push_back(v);
+            x /= 10;
         }
-        for(int v : num) {
-            if (ans > 0) {
-                ans *= 10;
-            }
-            ans += v;
-            if (ans > INT_MAX) return 0;
-        }
-
-        if (flag) {
-            ans = -ans;
-        }
-
-        if (ans < INT_MIN) {
-            return 0;
-        }
-        
         return static_cast<int>(ans);
     }
 };
