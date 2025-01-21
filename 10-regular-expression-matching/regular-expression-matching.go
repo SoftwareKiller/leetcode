3ceadd15bd93aa7dbae3231cc78e0a82b9af1634
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-01-21 15:32:09 +0800

    Time: 0 ms (100.00%) | Memory: 4.2 MB (76.95%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.go
+++ 10-regular-expression-matching/regular-expression-matching.go
@@ -1,17 +1,18 @@
 func isMatch(s string, p string) bool {
     n, m := len(s), len(p)
-    // dp[i][j]表示截至i的s与截至j的p是否匹配
-    dp := make([][]bool, n+1)
+    // dp[i][j]表示截至i的字符串s和截至j的匹配字符p是否相匹配
+    dp := make([][]bool, n + 1)
     for i := range dp {
-        dp[i] = make([]bool, m+1)
+        dp[i] = make([]bool, m + 1)
     }
     dp[0][0] = true
     for i := 0; i <= n; i++ {
         for j := 1; j <= m; j++ {
-            // 不对j进行边界判断是规则最后一条明确*之前必须有合法字符
+            // 对j的边界不做判断是因为规则最后一条为p[j-1]为*则之前必定有有效字符
             if p[j-1] == '*' {
-                // 
-                dp[i][j] = dp[i][j-2] || ( i > 0 && (s[i-1] == p[j-2] || p[j-2] == '.')) && dp[i-1][j]
+                // dp[i][j-2]判断当前*是否已经匹配结束
+                // dp[i-1][j]判断当前*的前一个字符是否匹配
+                dp[i][j] = dp[i][j-2] || (i > 0 && (s[i-1] == p[j-2] || p[j-2] == '.')) && dp[i-1][j]
             } else {
                 dp[i][j] = i > 0 && dp[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '.')
             }
