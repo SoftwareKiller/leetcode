9bbc2b9a0eb71d3cdb53479e953b03f07ddb0663
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-02-11 15:08:14 +0800

    Time: 21 ms (71.04%) | Memory: 23.8 MB (82.65%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -1,40 +1,45 @@
 class Solution {
-    std::unordered_map<std::string, unsigned int> map;
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        std::vector<int> result;
-        unsigned int length = words[0].size();
+        vector<int> ans;
+        if (s.length() == 0 || words.empty()) {
+            return ans;
+        }
+        unordered_map<string, int> hash;
+        for (auto& w : words) {
+            hash[w]++;
+        }
 
-        map.clear();
-        for (const std::string& word : words)
-            map[word]++;
-
-        for (unsigned int offset = 0; offset < length; ++offset) {
-            unsigned int size = 0;
-            std::unordered_map<std::string, unsigned int> seen;
-            for (unsigned int i = offset; i + length <= s.size(); i += length) {
-                std::string sub = s.substr(i, length);
-
-                auto itr = map.find(sub);
-                if (itr == map.end()) {
-                    seen.clear();
-                    size = 0;
+        int len = words[0].size();
+        int n = s.length();
+        for (int start = 0; start < len; start++) {
+            unordered_map<string, int> window;
+            int left = start;
+            int count = 0;
+            for (int r = start; r <= n - len; r += len) {
+                string sub = s.substr(r, len);
+                auto it = hash.find(sub);
+                if (it == hash.end()) {
+                    window.clear();
+                    count = 0;
+                    left = r + len;
                     continue;
                 }
 
-                ++seen[sub];
-                ++size;
-                while (seen[sub] > itr->second) {
-                    std::string first = s.substr(i - (size - 1) * length, length);
-                    --seen[first];
-                    --size;
+                window[sub]++;
+                count++;
+                while(window[sub] > hash[sub]) {
+                    string pop = s.substr(left, len);
+                    window[pop]--;
+                    count--;
+                    left += len;
                 }
-                
-                if (size == words.size())
-                    result.push_back(i - (size - 1) * length);
+
+                if (count == words.size()) {
+                    ans.push_back(left);
+                }
             }
         }
-
-        return result;
+        return ans;
     }
 };
