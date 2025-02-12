951c5e71cbed817b7aadcb5d8017de23aa04864f
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-02-12 13:28:36 +0800

    Time: 0 ms (100.00%) | Memory: 15.5 MB (92.38%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -3,18 +3,22 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size(), i = n - 2; 
         
+        // 从后向前找第一个升序数字
         while(i >= 0 && nums[i] >= nums[i+1]) {
             i--;
         }
 
         if (i >= 0) {
             int j = n - 1;
+            // 从后向前找第一个比升序数字大的数字
             while(nums[j] <= nums[i]) {
                 j--;
             }
+            // 把大数交换到低位
             swap(nums[i], nums[j]);
         }
 
+        // 对后续数组做逆序，小数在高位，大数在低位
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
