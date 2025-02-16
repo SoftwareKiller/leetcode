486fcc46af916adf7a756c1711c9bed6d94d0343
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-02-16 12:45:37 +0800

    Time: 2 ms (28.03%) | Memory: 4.9 MB (74.41%) - LeetSync

---
+++ 39-combination-sum/combination-sum.go
@@ -0,0 +1,26 @@
+func combinationSum(candidates []int, target int) [][]int {
+    ans := make([][]int, 0)
+    curr := make([]int, 0)
+    n := len(candidates)
+    var backtrace func(int, int, []int)
+    backtrace = func(t, start int, comb []int) {
+        if t == 0 {
+            temp := make([]int, len(comb))
+            copy(temp, comb)
+            ans = append(ans, temp)
+            return
+        }
+
+        if t < 0 {
+            return
+        }
+
+        for i := start; i < n; i++ {
+            comb = append(comb, candidates[i])
+            backtrace(t - candidates[i], i, comb)
+            comb = comb[:len(comb)-1]
+        }
+    }
+    backtrace(target, 0, curr)
+    return ans
+}
