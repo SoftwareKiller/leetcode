49e9531729c46d3d21aa568358bec6c4a1cfc35b
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-03-04 14:02:57 +0800

    Time: 0 ms (100.00%) | Memory: 11.4 MB (88.17%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> ans;
-        if (digits.back() != 9) {
-            ans = digits;
-            ans[ans.size()-1]++;
-            return ans;
-        }
-
         int n = digits.size();
-        int hit = 0;
         for (int i = n - 1; i >= 0; i--) {
-            if (digits[i] == 9) {
-                hit++;
-            } else {
-                break;
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
             }
-        }
 
-        if (hit == n) {
-            ans.resize(n+1, 0);
-            ans[0] = 1;
-            return ans;
+            digits[i] = 0;
         }
-
-        cout << hit;
-
-        ans = digits;
-        for (int i = 0; i < hit; i++) {
-            ans[n-i-1] = 0;
-        }
-        ans[n-hit-1]++;
-        return ans;
+        digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
