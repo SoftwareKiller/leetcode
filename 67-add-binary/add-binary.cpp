05ae760a75bbf16b72f6fe311c01ddb6638ca557
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-03-05 11:54:35 +0800

    Time: 0 ms (100.00%) | Memory: 8.9 MB (65.02%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -2,39 +2,15 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         string ans;
-        int aL = a.length() - 1, bL = b.length() - 1;
-        char carry = '0';
-        while(aL >= 0 || bL >= 0 || carry != '0') {
-            char s1 = '0', s2 = '0';
-            if (aL >= 0) {
-                s1 = a[aL--];
-            }
+        int carry = 0;
+        int al = a.length() - 1;
+        int bl = b.length() - 1;
+        while (al >= 0 || bl >= 0 || carry) {
+            if (al >= 0) carry += a[al--] - '0';
+            if (bl >= 0) carry += b[bl--] - '0';
 
-            if (bL >= 0) {
-                s2 = b[bL--];
-            }
-
-            char curr = '0';
-            if (s1 == '1' && s2 == '1') {
-                if (carry == '1') {
-                    curr = '1';
-                } else {
-                    curr = '0';
-                    carry = '1';
-                }
-            } else {
-                if (carry == '1') {
-                    if (s1 == '0' && s2 == '0') {
-                        curr = '1';
-                        carry = '0';
-                    }
-                } else {
-                    if (s1 == '1' || s2 == '1') {
-                        curr = '1';
-                    }
-                }
-            }
-            ans.push_back(curr);
+            ans += carry % 2 + '0';
+            carry /= 2;
         }
         reverse(ans.begin(), ans.end());
         return ans;
