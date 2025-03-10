ca953f810147d0df8064fb04b7e11b9d7f25c79e
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-03-10 23:04:55 +0800

    Time: 0 ms (100.00%) | Memory: 4.4 MB (12.75%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -0,0 +1,17 @@
+func findMin(nums []int) int {
+    l, r := 0, len(nums) - 1
+
+    for l < r {
+        mid := l + (r - l) >> 1
+
+        if nums[mid] < nums[r] {
+            r = mid
+        } else if nums[mid] > nums[r] {
+            l = mid + 1
+        } else {
+            r--
+        }
+    }
+
+    return nums[r]
+}
