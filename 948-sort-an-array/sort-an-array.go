3e653e99095c746a02c96b8844aaae3f66fe01d5
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-03-15 18:52:45 +0800

    Time: 27 ms (78.53%) | Memory: 11.3 MB (55.37%) - LeetSync

--- 948-sort-an-array/sort-an-array.go
+++ 948-sort-an-array/sort-an-array.go
@@ -1,51 +1,33 @@
 func sortArray(nums []int) []int {
-    mergeSort(nums, 0, len(nums) - 1)
+    n := len(nums)
+
+    for i := n/2 - 1; i >= 0; i-- {
+        adjust(nums, n, i)
+    }
+
+    for i := n - 1; i >= 0; i-- {
+        nums[0], nums[i] = nums[i], nums[0]
+        adjust(nums, i, 0)
+    }
+
     return nums
 }
 
-func mergeSort(nums []int, l, r int) {
-    if l >= r {
-        return
+func adjust(nums []int, n, i int) {
+    largest := i
+    l := 2 * i + 1
+    r := 2 * i + 2
+
+    if l < n && nums[l] > nums[largest] {
+        largest = l
     }
 
-    mid := ((r - l) >> 1) + l
-    mergeSort(nums, l, mid)
-    mergeSort(nums, mid + 1, r)
-
-    merge(nums, l, mid, r)
-}
-
-func merge(nums []int, l, mid, r int) {
-    i, j := l, mid + 1
-    curr := 0
-
-    t := make([]int, r - l + 1)
-
-    for i <= mid && j <= r {
-        if nums[i] < nums[j] {
-            t[curr] = nums[i]
-            i++
-        } else {
-            t[curr] = nums[j]
-            j++
-        }
-        curr++
+    if r < n && nums[r] > nums[largest] {
+        largest = r
     }
 
-    for i <= mid {
-        t[curr] = nums[i]
-        curr++
-        i++
-    }
-
-    for j <= r {
-        t[curr] = nums[j]
-        curr++
-        j++
-    }
-
-    i = l
-    for offset, n := range t {
-        nums[i+offset] = n
+    if largest != i {
+        nums[i], nums[largest] = nums[largest], nums[i]
+        adjust(nums, n, largest)
     }
 }
