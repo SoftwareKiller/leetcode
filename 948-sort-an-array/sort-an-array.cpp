9ba626c63803b713ff5fdd61c70bccb7a60eb56f
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-03-15 17:37:18 +0800

    Time: 229 ms (52.81%) | Memory: 146.7 MB (41.67%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,49 +1,48 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        int r = nums.size() - 1;
-        srand(time(nullptr));
-        quicksort(nums, 0, r);
+        mergeSort(nums, 0, nums.size() - 1);
         return nums;
     }
 
-    void quicksort(vector<int>& nums, int l, int r) {
+    void mergeSort(vector<int>& nums, int l, int r) {
         if (l >= r) {
             return;
         }
-        int i = partition(nums, l, r);
 
-        quicksort(nums, l, i - 1);
-        quicksort(nums, i + 1, r);
-    }
-
-    int midNum(vector<int>& nums, int l, int r) {
         int mid = ((r - l) >> 1) + l;
-        if (nums[l] <= nums[mid] && nums[mid] <= nums[r]) {
-            return mid;
-        }
+        mergeSort(nums, l, mid);
+        mergeSort(nums, mid + 1, r);
 
-        if (nums[mid] <= nums[l] && nums[l] <= nums[r]) {
-            return l;
-        }
-
-        return r;
+        merge(nums, l, mid, r);
     }
 
-    int partition(vector<int>& nums, int l, int r) {
-        int i = l, j = r;
-        int mid = midNum(nums, l, r);
-        swap(nums[l], nums[mid]);
-        while(i < j) {
-            // 结束后j右边的数都比nums[l]大
-            // while(i < j && nums[j] >= nums[l]) j--;
-            while(i < j && nums[j] >= nums[l]) j--;
-            // 结束后i左边的数都比nums[l]小
-            //while(i < j && nums[i] <= nums[l]) i++;
-            while(i < j && nums[i] <= nums[l]) i++;
-            swap(nums[i], nums[j]);
+    void merge(vector<int>& nums, int l, int mid, int r) {
+        int i = l, j = mid + 1, curr = 0;
+        vector<int> t(r - l + 1);
+
+        while (i <= mid && j <= r) {
+            if (nums[i] < nums[j]) {
+                t[curr] = nums[i];
+                i++;
+            } else {
+                t[curr] = nums[j];
+                j++;
+            }
+            curr++;
         }
-        swap(nums[l], nums[i]);
-        return i;
+
+        while(i <= mid) {
+            t[curr++] = nums[i++];
+        }
+
+        while(j <= r) {
+            t[curr++] = nums[j++];
+        }
+
+        i = l;
+        for (int n : t) {
+            nums[i++] = n;
+        }
     }
 };
