d399dea3233bd7664dc3b2a6227c234bb25ad1d7
Confidant. <31724286+SoftwareKiller@users.noreply.github.com>
2025-03-15 19:00:39 +0800

    Time: 22 ms (81.05%) | Memory: 12 MB (48.63%) - LeetSync

--- 948-sort-an-array/sort-an-array.go
+++ 948-sort-an-array/sort-an-array.go
@@ -1,10 +1,13 @@
 func sortArray(nums []int) []int {
     n := len(nums)
 
-    for i := n/2 - 1; i >= 0; i-- {
+    // 建堆
+    // 从最后一个叶子节点开始建堆
+    for i := n / 2 - 1; i >= 0; i-- {
         adjust(nums, n, i)
     }
 
+    // 排序
     for i := n - 1; i >= 0; i-- {
         nums[0], nums[i] = nums[i], nums[0]
         adjust(nums, i, 0)
@@ -15,8 +18,8 @@ func sortArray(nums []int) []int {
 
 func adjust(nums []int, n, i int) {
     largest := i
-    l := 2 * i + 1
-    r := 2 * i + 2
+    l := i * 2 + 1
+    r := i * 2 + 2
 
     if l < n && nums[l] > nums[largest] {
         largest = l
